d1daf1aff09971076a55666217d7b7a74194b8eb
komal-abjima <153979765+komal-abjima@users.noreply.github.com>
2024-02-28 13:33:07 +0530

    initial-commit(28-02-24)

--- First-Project/Program.cs
+++ First-Project/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Primitives;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,6 +8,16 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
+    //postman post 
+    StreamReader reader = new StreamReader(context.Request.Body);
+    string body = await reader.ReadToEndAsync();
+
+    Dictionary<string, StringValues> queryDict = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+
+    if (queryDict.ContainsKey("firstName")){
+        string firstName = queryDict["firstName"][0];
+        await context.Response.WriteAsync(firstName);
+    }
     //if(1 == 1)
     //{
     //    context.Response.StatusCode = 200;
@@ -20,16 +33,29 @@ app.Run(async (HttpContext context) =>
     //string path = context.Request.Path;
     //string method = context.Request.Method;
 
-    context.Response.Headers["Content-Type"] = "text/html";
+    //context.Response.Headers["Content-Type"] = "text/html";
 
-    if(context.Request.Method == "GET")
-    {
-        if (context.Request.Query.ContainsKey("id"))
-        {
-            string id = context.Request.Query["id"];
-            await context.Response.WriteAsync($"<p>{id}</p>");
-        }
-    }
+    //if(context.Request.Method == "GET")
+    //{
+    //    if (context.Request.Query.ContainsKey("id"))
+    //    {
+    //        string id = context.Request.Query["id"];
+    //        await context.Response.WriteAsync($"<p>{id}</p>");
+    //    }
+    //}
+
+    //context.Response.Headers["Content-type"] = "text/html";
+    //if(context.Request.Headers.ContainsKey("User-Agent")){
+    //    string UserAgent = context.Request.Headers["User-Agent"];
+    //    await context.Response.WriteAsync(UserAgent);
+    //}
+
+    //context.Response.Headers["Content-type"] = "text/html";
+    //if (context.Request.Headers.ContainsKey("AuthorizationKey"))
+    //{
+    //    string auth = context.Request.Headers["AuthorizationKey"];
+    //    await context.Response.WriteAsync(auth);
+    //}
 
     //await context.Response.WriteAsync($"<p>{path}</p>");
     //await context.Response.WriteAsync($"<p>{method}</p>");
