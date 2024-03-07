505ccfe37bdde8422fb8a95302ff1ec6288f6434
komal-abjima <153979765+komal-abjima@users.noreply.github.com>
2024-03-07 17:11:45 +0530

    initial-commit(07-03-24)

---
+++ Second_API_Project/Second_API_Project/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Second_API_Project.Entities;
+
+namespace Second_API_Project.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
---
+++ Second_API_Project/Second_API_Project/Entities/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace Second_API_Project.Entities
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
